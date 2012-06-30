4eaca4418dc46d0db05173129ad4116f559eba8c
Ugur Yoruk <uguryoruk21@gmail.com>
2012-06-30 17:52:20 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(name="dart",
+      version="0.1",
+      description="Django Articles App",
+      packages = find_packages(),
+      author="Ugur Yoruk",
+      author_email="madrize.24@gmail.com",
+      url="https://github.com/madrize/",
+      download_url="https://github.com/madrize/DART/")
